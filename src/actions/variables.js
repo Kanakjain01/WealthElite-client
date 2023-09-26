e5a8b4c483e48c3855f9d09ec930c545d6a8d3c9
Rishabh goswami <rishabhgoswami901@gmail.com>
2023-09-26 05:47:26 -0700

    add the live link of api

--- src/actions/variables.js
+++ src/actions/variables.js
@@ -2,7 +2,7 @@ import {CHANGE_VARIABLE} from "./type"
 // export const base_url = (process.env.NEXT_PUBLIC_ENDPOINT)
 
 // export const base_url = "https://dolphin-app-dxdkw.ondigitalocean.app"
-export const base_url = "http://localhost:3002/api"
+export const base_url = "https://wealthlife-api.onrender.com/api"
 export const changeVariable = (key, value) => {
 
     return ({
